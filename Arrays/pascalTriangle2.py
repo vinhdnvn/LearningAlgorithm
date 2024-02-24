91640e8cd72716dfab3e43101f2250ca9b77a023
vinhdnvn <vinh120203@gmail.com>
2024-02-25 00:04:00 +0700

    learn: pascalTriangle2

--- Arrays/pascalTriangle2.py
+++ Arrays/pascalTriangle2.py
@@ -8,3 +8,23 @@ class Solution:
                 output[j] += output[j-1]
         
         return output
+
+
+
+#          [1]
+#         [1, 1]
+#       [1, 2, 1]
+#      [1, 3, 3, 1]
+#    [1, 4, 6, 4, 1]
+    
+# Đưa về cùng 1 hàng coi như các array cho dễ hình dung
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# dễ dàng nhận thấy rằng mỗi index kế tiếp index đầu tiên của 1 rows sẽ tăng lên 1 đơn vị
+# và index cuối cùng của 1 rows sẽ luôn là 1
+# còn giữa 2 index đầu và cuối sẽ là tổng của 2 index trên nó
+# nên ta chỉ cần append 1 vào cuối mỗi rows và tính toán từ index cuối về index 1
+# và cứ như vậy cho đến khi đủ số hàng rowIndex
